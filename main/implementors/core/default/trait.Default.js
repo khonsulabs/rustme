6ae514195cdf588a1b0e957bbc8acf3381f0b09f
kl-botsu <botsu@khonsulabs.com>
2021-12-29 15:39:34 +0000

    Deploying to gh-pages from @ khonsulabs/rustme@f60cd45c2ab412f0a923856765a53cc96f9abb56 🚀

---
+++ main/implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["rustme"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rustme/struct.File.html\" title=\"struct rustme::File\">File</a>","synthetic":false,"types":["rustme::rustme::File"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
